23466f19c61410981534821e08dbdfa1dad3fd02
Maksim Sunoplya <969715@mail.ru>
2022-06-14 00:38:46 +0400

    Documented parameters

--- src/main/java/com/max/licensingservice/controller/LicenseController.java
+++ src/main/java/com/max/licensingservice/controller/LicenseController.java
@@ -4,6 +4,8 @@ import com.max.licensingservice.controller.constants.Urls;
 import com.max.licensingservice.model.License;
 import com.max.licensingservice.service.LicenseService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -22,32 +24,66 @@ public class LicenseController {
 
     private final LicenseService licenseService;
 
-    @Operation(summary = "Создание лицензии",
-            description = "Создать лицензию")
+    @Operation(summary = "Создание лицензии")
+    @Parameters({
+            @Parameter(
+                    name = "organizationId",
+                    description = "Идентификатор организации",
+                    required = true
+            )
+    })
     @PostMapping(Urls.Organization.OrganizationId.License.FULL)
     public ResponseEntity<String> create(@PathVariable Integer organizationId,
                                          @RequestBody License license) {
         return ResponseEntity.ok(licenseService.create(license, organizationId));
     }
 
-    @Operation(summary = "Получение лицензии",
-            description = "Получить лицензию")
+    @Operation(summary = "Получение лицензии")
+    @Parameters({
+            @Parameter(
+                    name = "organizationId",
+                    description = "Идентификатор организации",
+                    required = true
+            ),
+            @Parameter(
+                    name = "licenseId",
+                    description = "Идентификатор лицензии",
+                    required = true
+            )
+    })
     @GetMapping(Urls.Organization.OrganizationId.License.LicenseId.FULL)
     public ResponseEntity<License> find(@PathVariable Integer organizationId,
                                         @PathVariable Integer licenseId) {
         return ResponseEntity.ok(licenseService.find(organizationId, licenseId));
     }
 
-    @Operation(summary = "Обновление лицензии",
-            description = "Обновить лицензию")
+    @Operation(summary = "Обновление лицензии")
+    @Parameters({
+            @Parameter(
+                    name = "organizationId",
+                    description = "Идентификатор организации",
+                    required = true
+            )
+    })
     @PutMapping(Urls.Organization.OrganizationId.License.FULL)
     public ResponseEntity<String> update(@PathVariable Integer organizationId,
                                          @RequestBody License license) {
         return ResponseEntity.ok(licenseService.update(license, organizationId));
     }
 
-    @Operation(summary = "Удаление лицензии",
-            description = "Удалить лицензию")
+    @Operation(summary = "Удаление лицензии")
+    @Parameters({
+            @Parameter(
+                    name = "organizationId",
+                    description = "Идентификатор организации",
+                    required = true
+            ),
+            @Parameter(
+                    name = "licenseId",
+                    description = "Идентификатор лицензии",
+                    required = true
+            )
+    })
     @DeleteMapping(Urls.Organization.OrganizationId.License.LicenseId.FULL)
     public ResponseEntity<String> delete(@PathVariable Integer organizationId,
                                         @PathVariable Integer licenseId) {
