a7088e4b142615a0ee42a4be99e999ede8ec9e3e
Maksim Sunoplya <969715@mail.ru>
2022-06-13 21:02:37 +0400

    Documented LicenseController

--- src/main/java/com/max/licensingservice/controller/LicenseController.java
+++ src/main/java/com/max/licensingservice/controller/LicenseController.java
@@ -1,17 +1,38 @@
 package com.max.licensingservice.controller;
 
+import com.max.licensingservice.controller.constants.Urls;
+import com.max.licensingservice.model.License;
+import com.max.licensingservice.service.LicenseService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "License", description = "License API")
+@Tag(name = "Лицензии", description = "Управление лицензиями")
+@RequiredArgsConstructor
 @RestController
 public class LicenseController {
 
-    @Operation(description = "Тестовый метод")
-    @GetMapping("test")
-    public String test() {
-        return "Test";
+    private final LicenseService licenseService;
+
+    @Operation(summary = "Получение лицензии",
+            description = "Получить лицензию")
+    @GetMapping(Urls.Organization.OrganizationId.License.LicenseId.FULL)
+    public ResponseEntity<License> getLicense(@PathVariable Integer organizationId,
+                                              @PathVariable Integer licenseId) {
+        return ResponseEntity.ok(licenseService.getLicense(organizationId, licenseId));
+    }
+
+    @Operation(summary = "Обновление лицензии",
+            description = "Обновить лицензию")
+    @PutMapping(Urls.Organization.OrganizationId.License.FULL)
+    public ResponseEntity<String> updateLicense(@PathVariable Integer organizationId,
+                                                @RequestBody License license) {
+        return ResponseEntity.ok(licenseService.updateLicense(license, organizationId));
     }
 }
