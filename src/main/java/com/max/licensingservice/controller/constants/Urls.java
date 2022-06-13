44f58679bbde5df2d3dd612adceb2874ba3b43e4
Maksim Sunoplya <969715@mail.ru>
2022-06-13 13:21:31 +0400

    Added urls

--- src/main/java/com/max/licensingservice/controller/constants/Urls.java
+++ src/main/java/com/max/licensingservice/controller/constants/Urls.java
@@ -2,6 +2,7 @@ package com.max.licensingservice.controller.constants;
 
 public interface Urls {
 
+    String DELIMITER = "/";
     String SERVICE_NAME = "licensing-service";
     String SERVICE_DESCRIPTION = "Microservice - licensing service.";
     String ROOT = Api.NAME + "/" + Api.VERSION + "/" + SERVICE_NAME;
@@ -9,6 +10,25 @@ public interface Urls {
     interface Api {
         String NAME = "api";
         String VERSION = "v1";
+    }
 
+    interface Organization {
+        String PART = "organization";
+        String FULL = ROOT + DELIMITER + PART;
+
+        interface OrganizationId {
+            String PART = "{organizationId}";
+            String FULL = Organization.FULL + DELIMITER + PART;
+
+            interface License {
+                String PART = "license";
+                String FULL = OrganizationId.FULL + DELIMITER + PART;
+
+                interface LicenseId {
+                    String PART = "{licenseId}";
+                    String FULL = License.FULL + DELIMITER + PART;
+                }
+            }
+        }
     }
 }
