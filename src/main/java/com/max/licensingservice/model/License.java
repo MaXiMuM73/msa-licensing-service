c8899cf43d6307bea48e285af7a3b1b70454cef8
Maksim Sunoplya <969715@mail.ru>
2022-06-13 21:02:20 +0400

    Documented License

--- src/main/java/com/max/licensingservice/model/License.java
+++ src/main/java/com/max/licensingservice/model/License.java
@@ -1,15 +1,24 @@
 package com.max.licensingservice.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+@Schema(description = "Лицензия")
 @Data
 @Accessors(chain = true)
 public class License {
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY,
+            description = "Идентификатор", example = "1")
     private Integer id;
+    @Schema(description = "Идентификатор лицензии", example = "1")
     private Integer licenseId;
+    @Schema(description = "Описание", example = "Лицензия на 1 год")
     private String description;
+    @Schema(description = "Идентификатор организации", example = "1")
     private Integer organizationId;
+    @Schema(description = "Наименование продукта", example = "Хранилище данных")
     private String productName;
+    @Schema(description = "Тип лицензии", example = "Постоянная")
     private String licenseType;
 }
