c00c4cfeb04cbf5d2868d25cfa7539f240c6e16f
Maksim Sunoplya <969715@mail.ru>
2022-07-18 11:18:19 +0400

    Changed License entity

--- src/main/java/com/max/licensingservice/model/License.java
+++ src/main/java/com/max/licensingservice/model/License.java
@@ -1,27 +1,41 @@
 package com.max.licensingservice.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import lombok.experimental.Accessors;
-import org.springframework.hateoas.RepresentationModel;
 
-@Schema(description = "Лицензия")
+/**
+ * Сущность Лицензия.
+ */
 @Data
-@EqualsAndHashCode(callSuper = false)
+@Entity
+@Schema(description = "Лицензия")
 @Accessors(chain = true)
-public class License extends RepresentationModel<License> {
+@ToString
+public class License {
+
     @Schema(accessMode = Schema.AccessMode.READ_ONLY,
             description = "Идентификатор", example = "1")
-    private Integer id;
-    @Schema(description = "Идентификатор лицензии", example = "1")
-    private Integer licenseId;
+    @Id
+    @SequenceGenerator(name = "license_seq_gen", sequenceName = "license_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "license_seq_gen")
+    private Long id;
+
     @Schema(description = "Описание", example = "Лицензия на 1 год")
     private String description;
+
     @Schema(description = "Идентификатор организации", example = "1")
-    private Integer organizationId;
+    private Long organizationId;
+
     @Schema(description = "Наименование продукта", example = "Хранилище данных")
     private String productName;
+
     @Schema(description = "Тип лицензии", example = "Постоянная")
     private String licenseType;
 }
